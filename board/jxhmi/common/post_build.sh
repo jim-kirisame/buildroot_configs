85db38de5ddd6688764b7e8a58edbea606953806
jiangming1399 <jiangming1399@163.com>
2020-04-10 16:00:07 +0800

    移除 usb gadget 网卡的配置

--- board/jxhmi/common/post_build.sh
+++ board/jxhmi/common/post_build.sh
@@ -2,21 +2,6 @@
 
 SHELL_FOLDER=$(dirname "$0")
 
-
-cat > ${TARGET_DIR}/etc/network/interfaces << EOF
-
-auto lo
-iface lo inet loopback
-
-auto usb0
-    iface usb0 inet static
-    address 192.168.214.50
-    netmask 255.255.255.0
-    gateway 192.168.214.51
-
-EOF
-
-
 OVERLAY_SIZE=0x80000
 
 mkfs.jffs2 -s 0x100 -e 0x10000 --pad=$OVERLAY_SIZE -o ${BINARIES_DIR}/overlayfs.img -d $SHELL_FOLDER/overlayfs/
