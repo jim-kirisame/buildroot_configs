981c72c50ebf486b684f88a2810b769ea64c85fa
jiangming1399 <jiangming1399@163.com>
2020-04-21 23:46:23 +0800

    添加开机Logo

--- board/jxhmi/common/post_build.sh
+++ board/jxhmi/common/post_build.sh
@@ -5,3 +5,4 @@ SHELL_FOLDER=$(dirname "$0")
 OVERLAY_SIZE=0x80000
 
 mkfs.jffs2 -s 0x100 -e 0x10000 --pad=$OVERLAY_SIZE -o ${BINARIES_DIR}/overlayfs.img -d $SHELL_FOLDER/overlayfs/
+cp $SHELL_FOLDER/logo.bmp ${BINARIES_DIR}/logo.bmp
--- board/lotlab/common/post_build.sh
+++ board/lotlab/common/post_build.sh
@@ -20,3 +20,4 @@ EOF
 OVERLAY_SIZE=0x80000
 
 mkfs.jffs2 -s 0x100 -e 0x10000 --pad=$OVERLAY_SIZE -o ${BINARIES_DIR}/overlayfs.img -d $SHELL_FOLDER/overlayfs/
+cp $SHELL_FOLDER/logo.bmp ${BINARIES_DIR}/logo.bmp
