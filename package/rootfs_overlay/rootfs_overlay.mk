b78d08ba7b008d002ce430880437b966066a946a
jiangming1399 <jiangming1399@163.com>
2020-05-20 15:10:52 +0800

    将Overlay挂载脚本拆分到软件包中

---
+++ package/rootfs_overlay/rootfs_overlay.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# rootfs_overlay
+#
+################################################################################
+
+ROOTFS_OVERLAY_VERSION = 1.0
+ROOTFS_OVERLAY_SITE = $(ROOTFS_OVERLAY_PKGDIR)files
+ROOTFS_OVERLAY_SITE_METHOD = local
+ROOTFS_OVERLAY_LICENSE = Apache-2.0
+
+define ROOTFS_OVERLAY_INSTALL_TARGET_CMDS
+	cp $(@D)/mnt_overlay $(TARGET_DIR)/usr/sbin/
+	mkdir -p $(TARGET_DIR)/overlay
+endef
+
+$(eval $(generic-package))
